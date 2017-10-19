17c0e75740f5d5fb40cb5a6fe2da1cda72340bfd
Thomas Mercier <mercier.t@gmail.com>
2017-10-19 11:07:11 +0200

    core structure done

--- edward.py
+++ edward.py
@@ -6,11 +6,6 @@ def init_param(shape):
     initial = tf.random_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
-def bias_variable(shape):
-
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
 
 def compute_dist(vec):
 
@@ -41,30 +36,29 @@ def unit_output(parameters, origin):
 
     OC_x0 = A0_x0 + cos*AC_x1 - sin*AC_y1
     OC_y0 = A0_y0 + sin*AC_x1 - cos*AC_y1
-    OD_x0 = a*tf.cos(alpha) + d*tf.cos(beta+delta)
-    OD_y0 = a*tf.sin(alpha) + d*tf.sin(beta+delta)
+    OD_x0 = A0_x0 + sin*AD_x1 - cos*AD_y1
+    OD_y0 = A0_y0 + sin*AD_x1 - cos*AD_y1
 
-
-
-
-
-    return origin
+    return tf.concat( [OD_x0, OD_y0, OC_x0, OC_y0], axis=1 )
 
 
 n_units = 5
+n_points = 10
+R = 1
+initial_angle = np.pi / 3.
 
 sess = tf.InteractiveSession()
 
 units = []
 parameters = []
 
-output_target = tf.placeholder(tf.float32, shape=[None, 2])
-yB0 = tf.placeholder(tf.float32, shape=[None, 1])
-xB0 = tf.zeros(shape=[None, 1])
-xA0 = tf.zeros(shape=[None, 1])
-yA0 = tf.zeros(shape=[None, 1])
+output_initial_location = tf.constant( (R*cos(initial_angle), R*cos(initial_angle)), tf.float32 )
+output_radius = tf.constant( R, tf.float32 )
 
-units[0] = tf.concat( [xA0, yA0, xB0, yB0], axis=1 )
+B_input = tf.placeholder(tf.float32, shape=[n_points, 2])
+A_input = tf.zeros(shape=[n_points, 2])
+
+units[0] = tf.concat( [B_input, A_input], axis=1 )
 
 for i in range(n_units):
 
